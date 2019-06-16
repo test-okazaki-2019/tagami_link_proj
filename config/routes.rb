92770a6301194d995a5042e606e46786d79efce6
test-okazaki-2019 <okazaki2016new@gmail.com>
2019-06-17 07:39:55 +0900

    deviseの初期ファイルをインストール
    rails g devise:install
    
    メールサーバー設定を追加、仮のTOPページを設定
    
    rails g devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # FIXME: 仮のログイン後のTOPページ
+  root to: 'users#index'
+
   get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
